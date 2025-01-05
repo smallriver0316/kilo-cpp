0cfadd4d0a8c6614f2965d73144781def8e8b4cd
smallriver0316 <stream0316@gmail.com>
2025-01-05 16:19:49 +0900

    Added writing buffer as string

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#include <string>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
@@ -78,13 +79,58 @@ char editorReadKey()
   return c;
 }
 
+int getCursorPosition(int *rows, int *cols)
+{
+  char buf[32];
+  unsigned int i = 0;
+
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+  {
+    return -1;
+  }
+
+  while (i < sizeof(buf) - 1)
+  {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1)
+    {
+      break;
+    }
+
+    if (buf[i] == 'R')
+    {
+      break;
+    }
+
+    i++;
+  }
+
+  buf[i] = '\0';
+
+  if (buf[0] != '\x1b' || buf[1] != '[')
+  {
+    return -1;
+  }
+
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
+  {
+    return -1;
+  }
+
+  return 0;
+}
+
 int getWindowSize(int *rows, int *cols)
 {
   struct winsize ws;
 
   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
   {
-    return -1;
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+    {
+      return -1;
+    }
+
+    return getCursorPosition(rows, cols);
   }
   else
   {
@@ -96,23 +142,33 @@ int getWindowSize(int *rows, int *cols)
 
 /*** output ***/
 
-void editorDrawRows()
+void editorDrawRows(std::string &s)
 {
   int y;
   for (y = 0; y < E.screenrows; y++)
   {
-    write(STDOUT_FILENO, "~\r\n", 3);
+    s += "~\x1b[K";
+    if (y < E.screenrows - 1)
+    {
+      s += "\r\n";
+    }
   }
 }
 
 void editorRefreshScreen()
 {
-  write(STDOUT_FILENO, "\x1b[2J", 4);
-  write(STDOUT_FILENO, "\x1b[H", 3);
+  std::string s;
 
-  editorDrawRows();
+  s += "\x1b[?25l";
+  s += "\x1b[H";
 
-  write(STDOUT_FILENO, "\x1b[H", 3);
+  editorDrawRows(s);
+
+  s += "\x1b[H";
+  s += "\x1b[?25h";
+
+  write(STDOUT_FILENO, s.c_str(), s.size());
+  s = "";
 }
 
 /** input ***/
